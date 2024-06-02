f2fc081ba2400c10670207958e1efd818b93ff2c
mdeepanshu <deepanshumalviya8964@gmail.com>
2024-06-02 23:44:49 +0530

    deployed on firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-JxvUJC-_M0WQRR3DBhprC0t20cMNjII",
+  authDomain: "mandi-auction.firebaseapp.com",
+  projectId: "mandi-auction",
+  storageBucket: "mandi-auction.appspot.com",
+  messagingSenderId: "118523225331",
+  appId: "1:118523225331:web:3cab5f145b953b8a420386"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
