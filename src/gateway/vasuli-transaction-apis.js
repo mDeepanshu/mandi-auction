a0f75775426232ba7af6aafc9ae9884791bf3d90
mdeepanshu <deepanshumalviya8964@gmail.com>
2025-07-13 02:57:01 +0530

    whatsapp receipt for vasuli

--- src/gateway/vasuli-transaction-apis.js
+++ src/gateway/vasuli-transaction-apis.js
@@ -1,23 +1,46 @@
 import axiosHttp from "../interceptors/error-handling-interceptor";
 
 export const addVasuliTransaction = async (data) => {
-    try {
-        let recordsArray  = JSON.parse(localStorage.getItem('localObj'));
-        recordsArray.vasuli.push(data)
-        localStorage.setItem('localObj',JSON.stringify(recordsArray));
-        return 'success';
-    } catch (error) {
-        console.error('Error posting data:', error);
-        return 'error';
-    }
+  try {
+    let recordsArray = JSON.parse(localStorage.getItem("localObj"));
+    recordsArray.vasuli.push(data);
+    localStorage.setItem("localObj", JSON.stringify(recordsArray));
+    return "success";
+  } catch (error) {
+    console.error("Error posting data:", error);
+    return "error";
+  }
 };
 
 export const getOwedAmount = async (partyId) => {
-    try {
-        const response = await axiosHttp.get(`/party/getPartyById?partyId=${partyId}`);
-        return response;
-    } catch (error) {
-        console.error('Error posting data:', error);
-        return 'error';
-    }
-}
+  try {
+    const response = await axiosHttp.get(`/party/getPartyById?partyId=${partyId}`);
+    return response;
+  } catch (error) {
+    console.error("Error posting data:", error);
+    return "error";
+  }
+};
+export const whatsAppVasuli = async (vasuli) => {
+  try {
+    // const response = await axiosHttp.post(`/vyapari/notify-vasuli`, vasuli);
+    // return response;
+
+    const fakeResponse = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({data:{
+          responseCode: "200",
+          responseMessage: "Success",
+          responseBody:
+            '{"messaging_product":"whatsapp","contacts":[{"input":"8349842228","wa_id":"918349842228"}],"messages":[{"id":"wamid.HBgMOTE4MzQ5ODQyMjI4FQIAERgSNkE3NEFFNzUwNkQxQzg1RkFDAA==","message_status":"accepted"}]}',
+        }});
+      }, 1000);
+    });
+
+    return fakeResponse;
+  } catch (error) {
+    console.error("Error posting data:", error);
+    return "error";
+  }
+};
+
