119ab815b1ae53f391391238ad21700f834fdc89
mdeepanshu <deepanshumalviya8964@gmail.com>
2025-08-19 21:16:42 +0530

    wa to all in pending vasuli

--- src/dialogs/corformation/conformation.js
+++ src/dialogs/corformation/conformation.js
@@ -1,30 +1,31 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
 import styles from "./confirmation.module.css";
 
 export default function AlertDialog(props) {
-
   return (
     <React.Fragment>
       <Dialog
         open={props.open}
-        onClose={()=>props.handleClose(false)}
+        onClose={() => props.handleClose(false)}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            <div className={styles.text}>!! CONFIRM DELETE !!</div>
+            <div className={styles.text}>{props?.title ? <div>{props.title}</div> : <div>!! CONFIRM DELETE !!</div>}</div>
           </DialogContentText>
         </DialogContent>
-        <hr style={{padding:0,margin:0}} />
+        <hr style={{ padding: 0, margin: 0 }} />
         <DialogActions>
-          <Button color='error' onClick={()=>props.handleClose(true)}>Delete</Button>
-          <Button onClick={()=>props.handleClose(false)} autoFocus>
+          <Button variant="contained" color="error" onClick={() => props.handleClose(true)}>
+            {props?.btnText ? <div>{props.btnText}</div> : <div>Delete</div>}
+          </Button>
+          <Button variant="contained" color="primary" onClick={() => props.handleClose(false)} autoFocus>
             Cancel
           </Button>
         </DialogActions>
